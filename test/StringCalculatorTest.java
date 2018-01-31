03e3dd1bb44d0da5f03c259213a22a2e8631779d
Colton Garelli <Colton Garelli>
2018-01-30 22:00:13 -0500

    Test for adding two small numbers

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -16,12 +16,19 @@ class StringCalculatorTest extends StringCalculator{
         String testString1 = "1";
         int returned = Add(testString1);
         assertEquals(1,returned,"One number fail.");
+        returned = 0;
+        String largeTestString = "234892";
+        returned = Add(largeTestString);
     }
 
     @Test
     void addSmallNumbersTest(){
-
-
+        String testString = "4,2";
+        int returned = Add(testString);
+        assertEquals(6,returned,"Add failed to add two small numbers");
+        testString = "8,0";
+        returned = Add(testString);
+        assertEquals(8,returned,"Add failed to add 8 and 0");
     }
     @Test
     void addLargeNumbersTest(){
