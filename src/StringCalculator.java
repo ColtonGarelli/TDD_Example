2355f941dda14dc7b94efb6feec29ad495620f24
Austin Barrett <austinbarrett52@gmail.com>
2018-01-31 09:41:24 -0500

    included my solution to multiple nums in array

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,7 +1,9 @@
+
+
 public class StringCalculator implements StringCalculatorInterface{
 
     public int Add(String numbers) {
-        if(numbers.length()==0){
+        if(numbers == ""){
             return 0;
         }
         else if(!numbers.contains(",")) {
@@ -9,13 +11,26 @@ public class StringCalculator implements StringCalculatorInterface{
             return number;
         }
         else{
+            //Austin's solution
+            int sum = 0;
+            String[] output = numbers.split("\\,");
+
+            for (int i = 0; i<output.length;i++){
+                sum += Integer.valueOf(output[i]);
+            }
+
+            return sum;
+
+            /*
             String number1 = String.valueOf(numbers.substring(0,(numbers.indexOf(",")-1)));
             int firstNumber = Integer.parseInt(number1);
             String number2 = String.valueOf(numbers.substring(3));
             int secondNumber = Integer.parseInt(number2);
             int sum = firstNumber+secondNumber;
+            return sum;
+            */
         }
-        return 0;
+        //return 0;
     }
 }
     
